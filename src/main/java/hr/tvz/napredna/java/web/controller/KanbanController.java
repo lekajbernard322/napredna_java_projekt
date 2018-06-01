bb39887001ec0d9401f869eb6d5f54b9e83d9cc7
Bernard Lekaj <lekaj.bernard322@gmail.com>
2018-06-01 11:35:20 +0200

    Zadaci za prikaz na kanbanu se dohvacaju iz repozitorija.

--- src/main/java/hr/tvz/napredna/java/web/controller/KanbanController.java
+++ src/main/java/hr/tvz/napredna/java/web/controller/KanbanController.java
@@ -3,17 +3,26 @@ package hr.tvz.napredna.java.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hr.tvz.napredna.java.model.Zadatak;
+import hr.tvz.napredna.java.repository.ZadatakRepository;
+
 @Controller
 @RequestMapping("/kanban")
 public class KanbanController {
 	
+	@Autowired
+	private ZadatakRepository zadatakRepository;
+	
 	@GetMapping
 	public String mainView(Model model) {
+		
+		List<Zadatak> zadaci = zadatakRepository.findAll();
 		
 		List<String> stanja = new ArrayList<>();
 		stanja.add("To do");
@@ -21,52 +30,10 @@ public class KanbanController {
 		stanja.add("Testing");
 		stanja.add("Done");
 		
-		List<Zadatak> zadaci = new ArrayList<>();
-		for (int i = 1; i < 10; i++) {
-			Zadatak zadatak = new Zadatak();
-			zadatak.setId(i);
-			zadatak.setNaslov("Zadatak" + i);
-			zadatak.setOpis("Lorem ipsum dolor sit amet, consectetur adipiscing elit." + i);
-			zadatak.setStanje(stanja.get(i % stanja.size()));
-			zadaci.add(zadatak);
-		}
-		
 		model.addAttribute("zadaci", zadaci);
 		model.addAttribute("stanja", stanja);
 		
 		return "kanban/ploca";
-	}
-	
-	static class Zadatak {
-		private Integer id;
-		private String naslov;
-		private String opis;
-		private String stanje;
-		
-		public Integer getId() {
-			return id;
-		}
-		public void setId(Integer id) {
-			this.id = id;
-		}
-		public String getNaslov() {
-			return naslov;
-		}
-		public void setNaslov(String naslov) {
-			this.naslov = naslov;
-		}
-		public String getOpis() {
-			return opis;
-		}
-		public void setOpis(String opis) {
-			this.opis = opis;
-		}
-		public String getStanje() {
-			return stanje;
-		}
-		public void setStanje(String stanje) {
-			this.stanje = stanje;
-		}
 	}
 	
 }
