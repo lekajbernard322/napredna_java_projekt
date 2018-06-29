2c84bba92c3482e1819c1b346a24d5fd31e57c5c
Bernard Lekaj <lekaj.bernard322@gmail.com>
2018-06-29 18:47:20 +0200

    Adding stream to job.

--- src/main/java/hr/tvz/napredna/java/config/QuartzZadatak/KorisniciJob.java
+++ src/main/java/hr/tvz/napredna/java/config/QuartzZadatak/KorisniciJob.java
@@ -1,19 +1,14 @@
 package hr.tvz.napredna.java.config.QuartzZadatak;
 
 
-import hr.tvz.napredna.java.model.Korisnik;
-import hr.tvz.napredna.java.repository.ZadatakRepository;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.User;
-
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 public class KorisniciJob extends QuartzJobBean {
 
@@ -23,23 +18,13 @@ public class KorisniciJob extends QuartzJobBean {
 
     public void executeInternal(JobExecutionContext jec){
 
-        List<Object> principals = sessionRegistry.getAllPrincipals();
-
-        List<String> usersNamesList = new ArrayList<String>();
-
-        System.out.print("Trenutno prijavljeni korisnici (" + new Date().toString() + "): ");
-        Iterator korisnici = principals.iterator();
-        while( korisnici.hasNext() ){
-            Object principal = korisnici.next();
-            if (principal instanceof User) {
-                usersNamesList.add(((User) principal).getUsername());
-                System.out.print(((User) principal).getUsername());
-            }
-            if( korisnici.hasNext() )
-                System.out.print(", ");
-        }
-        System.out.println();
-
+        String usernames = sessionRegistry
+        		.getAllPrincipals()
+        		.stream()
+        		.map(p -> ((User) p).getUsername())
+        		.collect(Collectors.joining(","));
+        System.out.println("Trenutno prijavljeni korisnici(" 
+        		+ new Date().toString() + "): " + usernames);
     }
 
 
